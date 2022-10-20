299c74da2f6f5dfb2ff0e472d16556fc9f7fb13c
SaadTahir28 <saadtahir28@gmail.com>
2022-10-20 22:51:43 +0500

    Update QuranScreen.js

--- Screens/QuranScreen.js
+++ Screens/QuranScreen.js
@@ -4,7 +4,7 @@ import {Text, View } from 'react-native';
 export function QuranScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Quran!</Text>
+        <Text>Quran Screen!</Text>
       </View>
     );
   }
