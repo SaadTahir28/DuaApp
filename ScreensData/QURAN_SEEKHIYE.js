44faceba2984a48d0a4e25a1cc310f549e0f40b5
Saad Tahir <saadtahir28@gmail.com>
2023-01-03 17:13:11 +0500

    Added data

---
+++ ScreensData/QURAN_SEEKHIYE.js
@@ -0,0 +1,33 @@
+export const quran_seekhiye = `
+۔ قرآنی تجوید کی اہمیت1سبق نمبر
+Lesson No 1 https://youtu.be/RiN0F0Tt5ow۔ قرآنی حروف مفردات کی ادائیگی2سبق نمبر
+Lesson No 2 https://youtu.be/i-_f1c97TWc۔موٹی آواز والے حروف کی پہچان3سبق نمبر
+Lesson No 3 https://youtu.be/qGZVfGejvCI۔ملتی جلتی آوازوں والے حروف کی ادائیگی4سبق نمبر
+Lesson No 4 https://youtu.be/sg-oIatDP68
+۔ملتی جلتی آوازوں والے حروف کی مزید پہچان5سبق نمبر
+Lesson No 5 https://youtu.be/1MCiy10T_8M بنیادی فرق4۔ ملتی جلتی آوازوں میں 6سبق نمبر
+Lesson No 6 https://youtu.be/PUahtM-0OdU۔ حرکات کی ادائیگی کی پہچان7سبق نمبر
+Lesson No 7 https://youtu.be/Nbc9P4QwCwQ۔حرکات (پڑی زیر کھڑی زیر کی ادائیگی پہچان 8سبق نمبر
+(Lesson No 8 https://youtu.be/TFu0Qt2ZFR8
+۔ سیدھی پیش الٹی پیش کی آسان آدائیگی9سبق نمبر
+Lesson No 9 https://youtu.be/wM46pUMdfJs۔ تنوین کسے کہتے اور اس کی ادائیگی10سبق نمبر
+Lesson No 10 https://youtu.be/G7twrHdMroI۔ حروف قلقلہ کی اقسام کی پہچان11سبق نمبر
+Lesson No 11https://youtu.be/NA76p2vb27k۔ حروف مده کی ادئیگی کا آسان حل12سبق نمبر
+Lesson No 12https://youtu.be/HoVX1ho9-rI
+۔ حروف لین کی ادائیگی کی پہچان13سبق نمبر
+Lesson No 13https://youtu.be/ldnpMm5eGW8۔حروف مده حروف لین کی ادائیگی میں فرق14سبق نمبر
+Lesson No 14https://youtu.be/_1VAFs8QyJc۔ جزم یا سکون ادائیگی کا آسان حل15سبق نمبر
+Lesson No 15https://youtu.be/s-Hebx8y5Oc۔ شد کی ادائیگی اس کی پہچان16سبق نمبر
+Lesson No 16https://youtu.be/HZdAdWurTYk
+۔ مدآت کی اقسام اور ان کی پہچان17سبق نمبر
+Lesson No 17https://youtu.be/3Cy0T7h9IVg۔ حروف مقطعات کی ادائیگی اور پہچان18سبق نمبر
+Lesson No 18https://youtu.be/aoBHVnygCxM۔ حروف شبہ مستعلیہ کی پہچان19سبق نمبر
+Lesson No 19https://youtu.be/lIekascYCtQ۔ ر کی موٹی اورباریک حالتیں20سبق نمبر
+Lesson No 20
+https://youtu.be/BTyi93fMhqU۔ الف کب موٹا کب باریک ہوگا21سبق نمبر
+Lesson No 21https://youtu.be/bI3I1L7OaqI۔غنہ وعدم غنہ کے احکام22سبق نمبر
+Lesson No 22https://youtu.be/mHuyHX1QeJg۔قرآنی مخصوص اھم الفاظ کی پہچان23سبق نمبر
+Lesson No 23https://youtu.be/WVF6fyHyW5o۔ نون قطنی کسے کہتے ھیں24سبق نمبر
+Lesson No 24
+https://youtu.be/ofJkmA2d3qU۔ قرآن مجید میں وقف کے طریقے اور انکی 25سبق نمبر
+ادائیگیLesson No 25https://youtu.be/wiu2PjWEqUEThe End`
